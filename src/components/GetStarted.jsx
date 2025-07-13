9e7776eaaa6df3d5b5fe5a362b4af815f1752702
bilal-waleed <bilalwaleed246@gmail.com>
2025-07-14 00:56:02 +0500

    update path

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useTheme } from '../context/ThemeContext';
-import { Link } from 'react-router';
+import { Link } from 'react-router-dom';
 
 const GetStarted = () => {
   const { theme } = useTheme();
